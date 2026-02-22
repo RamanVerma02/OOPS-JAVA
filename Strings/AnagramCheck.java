005f8a50d1a80089aa4a24038eba1da4214f6660
Raman verma <raman02verma@gmail.com>
2026-02-23 00:32:48 +0530

    added Anagram string progrram

---
+++ Strings/AnagramCheck.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String a = "silent";
+        String b = "listen";
+
+        char arr1[] = a.toCharArray();
+        char arr2[] = b.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if(Arrays.equals(arr1, arr2))
+            System.out.println("Anagram");
+        else
+            System.out.println("Not Anagram");
+
+    }
+}

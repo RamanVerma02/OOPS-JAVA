4acf4cc38babf9827cc5490e2b3668914906f0cf
Raman verma <raman02verma@gmail.com>
2026-02-23 00:24:20 +0530

    added Palindrome String program

---
+++ Strings/Palindrom.java
@@ -0,0 +1,17 @@
+public class Palindrom {
+    public static void main(String[] args) {
+        String str = "moon";
+        String rev = "";
+
+        for(int i=str.length()-1; i>=0; i--){
+            rev += str.charAt(i);
+        }
+
+        if(rev.equals(str)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+
+    }
+}

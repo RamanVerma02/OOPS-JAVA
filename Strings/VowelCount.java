407ca9334dc8237074b8bb98aafda940559acdc7
Raman verma <raman02verma@gmail.com>
2026-03-05 23:33:01 +0530

    Add vowel count program in string

---
+++ Strings/VowelCount.java
@@ -0,0 +1,14 @@
+public class VowelCount {
+    public static void main(String[] args) {
+        String str = "rama apple king";
+        int count = 0;
+
+        for(int i=0; i<str.length(); i++){
+            char ch = Character.toLowerCase(str.charAt(i));
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count++;
+            }
+        }
+        System.err.println("Vowels: "+count);
+    }
+}

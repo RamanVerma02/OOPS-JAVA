bfb5bb635bf007098100c1c1cfcd2150e82bec09
Raman verma <raman02verma@gmail.com>
2026-03-06 23:24:38 +0530

    Added TypeCasting program

---
+++ Java Basics/TypeCasting.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		// TypeCasting float to int
+		int num = (int)23.6873f;
+		System.out.println(num);
+		
+		// int to float
+		float no = (float) 9338;
+		System.out.println(no);
+
+
+	}
+}

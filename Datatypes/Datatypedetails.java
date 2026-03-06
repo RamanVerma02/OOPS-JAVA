28d2f9a9adc4378f79c61d9598963c648f0f8102
Raman verma <raman02verma@gmail.com>
2026-03-06 23:16:49 +0530

    Added about data types

---
+++ Datatypes/Datatypedetails.java
@@ -0,0 +1,12 @@
+public class Datatypedetails {
+    public static void main(String args[]){
+        int x = 5;
+        // if you want to print this number in the form of binary no. then use -->
+
+        System.out.println(Integer.toBinaryString(x));
+
+        int y = -5;
+        // output will be in 2's complpiment of binary form
+        System.out.println(Integer.toBinaryString(y));
+    }
+}

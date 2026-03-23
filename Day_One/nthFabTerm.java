7ddee979918aa67a8b94c074b8fa608253e0b066
Raman verma <raman02verma@gmail.com>
2026-03-23 10:18:45 +0530

    Added nth term in fab Program

---
+++ Day_One/nthFabTerm.java
@@ -0,0 +1,26 @@
+public class nthFabTerm {
+    public static int fab(int n){
+        if(n==0 || n==1) return n;
+        if(n<=1) return n;
+
+        return fab(n-1)+fab(n-2);
+    }
+    public static int fabMethod2(int n){
+        if(n<=1) return n;
+
+        int a= 0;
+        int b= 1;
+        
+        for(int i=2; i<=n; i++){
+            int temp = a+b;
+            a=b;
+            b=temp;
+        }
+        return b;
+        
+    }
+    public static void main(String[] args) {
+        // System.out.println(fab(6));
+        System.out.println(fabMethod2(50));
+    }
+}
